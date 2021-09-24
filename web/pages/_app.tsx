e20d40b6402cb6faad683527b2188662883011ba
Lanuza Santos <lanuzasantos1432@gmail.com>
2021-09-24 11:45:00 -0300

    att homecontext

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { HomeContextProvider } from '../Context/HomeContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   <HomeContextProvider> 
+  <Component {...pageProps} />
+  </HomeContextProvider>
+  )
 }
 
 export default MyApp
