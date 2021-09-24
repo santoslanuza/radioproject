45bd372a58448e1d001aa70cfc1ebfb0dc057fd5
Lanuza Santos <lanuzasantos1432@gmail.com>
2021-09-24 15:11:57 -0300

    homecontext

--- web/Context/HomeContext.tsx
+++ web/Context/HomeContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, ReactNode, useDebugValue } from "react";
+import { Children, createContext, ReactNode, useDebugValue, useEffect, useState } from "react";
 import { getConstantValue } from "typescript";
+import Home from "../pages";
 type HomeContextData = {
-
+    audio: HTMLAudioElement;
 }
 
 type HomeContextProps = {
@@ -11,9 +12,20 @@ type HomeContextProps = {
 export const HomeContext = createContext({} as HomeContextData); 
 
 export const HomeContextProvider = ({children}: HomeContextProps) => {
-     return {
-         HomeContext
+    const  [audio, setAudio] = useState<HTMLAudioElement>();
+    
+    useEffect{()=>{
+      const initialAudio  = new Audio();
+    }; []};
 
-     };
+    return ( 
+         <HomeContext.Provider value={
+            {
+              audio
+            }
+        }>
+         {Children}
+    </HomeContext.Provider>
 
+     );
 }
