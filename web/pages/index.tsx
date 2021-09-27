7432af7f43ad7b86d3ea80fe516f3484041fc762
Lanuza Santos <lanuzasantos1432@gmail.com>
2021-09-27 13:14:30 -0300

    index.tsx

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import { useContext } from 'react'
 import { HomeContext } from '../Context/HomeContext';
 import styles from '../styles/Home.module.css'
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       {isPlaying ?
-      (<button onClick={playPause}>Pausar</button>):
-      (<button onClick={playPause}>Tocar</button>)    
+(<button onClick={playPause}><PauseCircleOutlineIcon /></button>):
+      (<button onClick={playPause}><PlayCircleOutlineIcon /></button>)    
      }    
      </div>
   )
