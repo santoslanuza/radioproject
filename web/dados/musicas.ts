f641b7988f97e0eb89ea25d4f72996e8e2a87984
Lanuza Santos <lanuzasantos1432@gmail.com>
2021-09-28 12:44:34 -0300

    dados

---
+++ web/dados/musicas.ts
@@ -0,0 +1,17 @@
+export const musicas = [
+    {
+        nome: "Doja Cat - Woman",
+        audio: "audio1.mp3",
+        capa: "audio1.png"
+    },
+    {
+        nome: "Bruna Karla - Deus vem me socorrer",
+        audio: "audio2.mp3",
+        capa: "audio2.png"
+    },
+    {
+        nome: "Pabllo Vittar - A lua",
+        audio: "audio3.mp3",
+        capa: "audio3.png"
+    }
+]
