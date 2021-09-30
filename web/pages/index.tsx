3041466c20c9cb44e06ec5ea333854f127b34c39
Lanuza Santos <lanuzasantos1432@gmail.com>
2021-09-30 16:30:36 -0300

    index.tsx

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,17 +7,34 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   const {
      isPlaying,
-     playPause
+     musicas,
+     musicIndex,
+     playPause,
+     configMusic
   } = useContext(HomeContext);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-      
+      <div className={styles.headerImage}> 
+       <img src={`capas/${musicas[musicIndex].capa}`} />    
       {isPlaying ?
       (<button onClick={playPause}><PauseCircleOutlineIcon /></button>):
       (<button onClick={playPause}><PlayCircleOutlineIcon /></button>)    
      }  
-     </div>  
+     </div>
+     </div>
+      <div className={styles.content}>
+        {
+          musicas.map((music, index)=> {
+          return (
+            <div onClick={() => configMusic(index)} key={music.audio} className={styles.musicItem}>
+            <img src={`capas/${music.capa}`} />
+            <h1>{music.nome}</h1>
+           </div>
+          )
+        }) 
+      } 
+      </div>
      </div>
   )
 }
